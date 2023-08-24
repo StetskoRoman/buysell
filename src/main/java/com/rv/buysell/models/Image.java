73b182cbe74befd813c5bced62a33d6fc822f864
Roman Stetsko <asgzxba@mail.ru>
2023-08-24 15:13:09 +0300

    Added photo possibility, should fix invisibility in photo displaying

---
+++ src/main/java/com/rv/buysell/models/Image.java
@@ -0,0 +1,31 @@
+package com.rv.buysell.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "images")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String originalFileName;
+    private Long size;
+    private String contentType;
+    private boolean isPreviewImage;
+
+    @Lob
+    private byte [] bytes;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Product product;
+
+
+}
